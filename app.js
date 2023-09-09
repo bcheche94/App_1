f404d3fca2db05b4cf9301c5c45d970b12ea0c21
bcheche94 <143166864+bcheche94@users.noreply.github.com>
2023-09-09 14:52:54 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// lots of code is missing from this file, this was done on purpose in order to prevent
+// stealing of app by whoever is looking
+// the purpose of this github repo is to show an employer that I can code,
+// and most of the code has been uploaded
